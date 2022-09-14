a5bde1fa2bde3aca21545dd5d1f37a9559b42f68
Emiralp Ülger <emiralprc@gmail.com>
2022-09-14 18:28:37 +0300

    Website Controller 1.0

--- 02-day.py
+++ 02-day.py
@@ -1,1 +1,16 @@
-print("day 2s")
+import requests
+
+def OnSite():
+    '''Check if website is connected to internet'''
+    try:
+        requests.get("http://www.emiralp.site")
+        return True
+    
+    except requests.ConnectionError:
+        return False
+
+if OnSite():
+    print("Internet Access")
+
+else:
+    print('No Internet')
